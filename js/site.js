66658b3d65540e4c38346e61a7aee4b0d8d5e0fa
Benci Magpoc <BENCIIAN@GMAIL.COM>
2022-04-14 20:41:30 -0400

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,29 +1,97 @@
-function getMessage() {
-    let message = document.getElementById("txtMessage").value;
-    displayMessage(message);
+//get the loans from page
+function getValues() {
+
+    let loanData = {
+        amount: 0.00,
+        term: 0,
+        rate: 0
+    };
+
+    loanData.amount = parseFloat(document.getElementById("loanAmount").value);
+    loanData.term = parseInt(document.getElementById("loanTerms").value);
+    loanData.rate = parseFloat(document.getElementById("loanRate").value);
+    console.log(loanData.amount);
+
+
+    let calculatedLoan = calculateLoanTotal(loanData);
+
+    displayLoanData(calculatedLoan);
+    //call buildSchedule
+    //buildSchedule();
+
+    //displayData();
 }
 
+//Logic Layer
+function calculateLoanTotal(loanData) {
 
-function displayMessage(message) {
+    loanData.remainingBalance = loanData.amount;
 
-    // let item = `<li class="list-group-item">${message}</li>`;
+    loanData.totaInterest = 0
 
-    // getting ol element from the page
-    element = document.getElementById("results");
-
-    // create a new li element
-    let item = document.createElement("li");
-    //add class to the element
-    item.classList.add("list-group-item");
-    item.classList.add("messageClass");
-
-    //set the message for the li element
-    item.innerHTML = message;
-    // add new item to list
-    element.appendChild(item);
+    for (let index = loanData.term; index >= 1; index--) {
 
 
-    // element.insertAdjacentHTML("beforeend", item);
-    let clearForm = document.getElementById("txtMessage");
-    clearForm.innerHTML = "";
+        loanData.totalMonthlyPayment = (loanData.remainingBalance * (loanData.rate / 1200)) / (1 - (1 + loanData.rate / 1200) ** -index);
+
+        loanData.interestPayment = loanData.remainingBalance * loanData.rate / 1200;
+
+        loanData.principalPayment = loanData.totalMonthlyPayment - loanData.interestPayment;
+
+        loanData.remainingBalance -= loanData.totalMonthlyPayment;
+
+        loanData.totaInterest += loanData.interestPayment;
+
+        let loanTable = document.getElementById("paymentData-table");
+
+        let paymentDataRow = loanTable.insertRow(loanData.term - index + 1);
+
+
+        let columnMonths = paymentDataRow.insertCell(0);
+        columnMonths.innerHTML = loanData.term - index + 1;
+
+        let columnPayment = paymentDataRow.insertCell(1);
+        columnPayment.innerHTML = loanData.totalMonthlyPayment;
+
+        let columnPrincipal = paymentDataRow.insertCell(2);
+        columnPrincipal.innerHTML = loanData.principalPayment;
+
+        let columnInterest = paymentDataRow.insertCell(3);
+        columnInterest.innerHTML = loanData.interestPayment;
+
+        let columnTotalInterest = paymentDataRow.insertCell(4);
+        columnTotalInterest.innerHTML = loanData.totaInterest;
+
+        let columnBalance = paymentDataRow.insertCell(5);
+        columnBalance.innerHTML = loanData.remainingBalance;
+
+        // loanTableRow.childNodes[0].innerHTML = loanData.term - index + 1;
+        // loanTableRow.childNodes[1].innerHTML = loanData.totalMonthlyPayment;
+        // loanTableRow.childNodes[2].innerHTML = loanData.principalPayment;
+        // loanTableRow.childNodes[3].innerHTML = loanData.interestPayment;
+        // loanTableRow.childNodes[4].innerHTML = loanData.totaInterest;
+        // loanTableRow.childNodes[5].innerHTML = loanData.remainingBalance;
+
+    }
+
+    // console.log(loanData.totalMonthlyPayment);
+    console.log(loanData);
+    return loanData;
 }
+
+function displayLoanData(loanData) {
+    document.getElementById("totalPrincipal").innerHTML = loanData.amount;
+    document.getElementById("totalInterest").innerHTML = loanData.amount;
+    document.getElementById("totalCost").innerHTML = loanData.amount;
+    document.getElementById("totalMonthlyPayment").innerHTML = loanData.totalMonthlyPayment;
+}
+// //builds ammortization schedule
+// function buildSchedule(amount, rate, term, payment) {
+//     //return array of payment objects
+// }
+
+// //display table of payments
+// //and summary info at the top of the page
+// function displayData() {
+
+// }
